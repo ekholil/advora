e9ec68ce206a0d24395d37fe206cd7672807c722
ekholil <kholilebrahim01@gmail.com>
2022-01-06 16:33:56 +0600

    first commit

---
+++ src/Components/Cards.js
@@ -0,0 +1,160 @@
+import React, { useEffect, useState } from "react";
+import SingleProduct from "./SingleProduct";
+import Slider from "react-slick";
+const Cards = () => {
+  const [products, setProducts] = useState([]);
+  const [displayProducts, setDisplayProducts] = useState([]);
+  const [selectedOption, setSelectedOption] = useState("Products");
+  const settings = {
+    className: "center",
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 4,
+    swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+    afterChange: function (index) {
+      console.log(
+        `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
+      );
+    },
+  };
+  // extract single product from array
+  const productNames = products.map((item) => item.product_name);
+  //find unique product names
+  const uniqueProducts = [...new Set(productNames)];
+  // extract single state from array
+  const states = products.map((item) => item.address.state);
+  //find unique state names
+  const uniqueStates = [...new Set(states)];
+  // extract city state from array
+  const cities = products.map((item) => item.address.city);
+  //find unique state names
+  const uniqueCities = [...new Set(cities)];
+
+  useEffect(() => {
+    fetch("https://assessment-edvora.herokuapp.com")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+        setDisplayProducts(data);
+      });
+  }, []);
+  // handle product name change from dropdown menu
+  const handleNameChange = (selected) => {
+    setSelectedOption(selected);
+    const selectedProduct = products.filter(
+      (product) => product?.product_name === selected
+    );
+    setDisplayProducts(selectedProduct);
+  };
+  // handle state change
+  const handleStateChange = (selected) => {
+    setSelectedOption(selected);
+    const selectedProduct = displayProducts.filter(
+      (product) => product?.address.state === selected
+    );
+    setDisplayProducts(selectedProduct);
+  };
+  // handle City change
+  const handleCityChange = (selected) => {
+    setSelectedOption(selected);
+    const selectedProduct = displayProducts.filter(
+      (product) => product?.address.city === selected
+    );
+    setDisplayProducts(selectedProduct);
+  };
+  return (
+    <div className="container-fluid pt-5">
+      <div className="row">
+        <div className="col-md-2">
+          <div
+            style={{ backgroundColor: "#131313" }}
+            className="box py-4 px-1 rounded"
+          >
+            <h4 className="text-white">Filters</h4>
+            <hr className="text-white" />
+            {/* select name */}
+            <select
+              onChange={(e) => handleNameChange(e.target.value)}
+              value={selectedOption}
+              id="brand_names"
+              className="mb-3"
+            >
+              {uniqueProducts.map((item) => (
+                <option value={item}> {item}</option>
+              ))}
+            </select> <br />
+
+            {/* select State */}
+            <select
+              onChange={(e) => handleStateChange(e.target.value)}
+              value={selectedOption}
+              id="brand_names"
+              className="mb-3"
+            >
+              {uniqueStates.map((item) => (
+                <option value={item}> {item}</option>
+              ))}
+            </select> <br />
+            {/* select City */}
+            <select
+              onChange={(e) => handleCityChange(e.target.value)}
+              value={selectedOption}
+              id="brand_names"
+              className="mb-3"
+            >
+              {uniqueCities.map((item) => (
+                <option value={item}> {item}</option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className="col-md-10">
+          <div className="row">
+            <h2 className="text-white">Edvora</h2>
+            <h3 className="text-secondary">Products</h3>
+            <h4 className="text-white">Product Name</h4>
+            <hr />
+            <div
+              style={{ backgroundColor: "#131313" }}
+              className="py-2 ps-0 pe-2 row rounded d-flex justify-content-center"
+            >
+              <Slider {...settings}>
+                {displayProducts.map((product) => (
+                  <SingleProduct key={product.time} product={product} />
+                ))}
+              </Slider>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
---
+++ src/Components/SingleProduct.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const SingleProduct = (props) => {
+    const {product_name, brand_name, address, price, date, image, discription} = props.product
+    return (
+        <div style={{boxSizing:'border-box', backgroundColor: '#232323'}} className="rounded  text-white px-3 py-2 m-2 single_product">
+            <div className='d-flex'>
+                <div className='p-2'>
+                    <img style={{objectFit:'cover', width: '70px', height:'70px'}} className='img-fluid rounded' src={image} alt="" />
+                </div>
+                <div className="p-1">
+                    <p className='m-0 fw-bold'>{product_name}</p>
+                    <p className='m-0 text-secondary'>{brand_name}</p>
+                    <p className='m-0 fw-bold'>$ {price}</p>
+                </div>
+            </div>
+            <div className='d-flex justify-content-between text-secondary'>
+                <small>{address.city}</small>
+                <small>Date: {date.substr(0,10)}</small>
+            </div>
+            <span className='text-secondary'>{discription}</span>
+        </div>
+    );
+};
+
+export default SingleProduct;
